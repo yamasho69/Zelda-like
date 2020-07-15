4e2d59b184bf265469f57f9f86ab74fd006bb758
yamasho69 <nitiham55@yahoo.co.jp>
2020-07-16 00:36:50 +0900

    20200716 001
    剣を振るモーション及びコライダーを追加

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Part11で追加。現在のプレイヤーの状態を判別
+public enum PlayerState {
+    walk,
+    attack,
+    interact
+}
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerState currentState;//Part11で追加。主人公の状態。walk,attak,interact
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
@@ -17,12 +24,32 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
+    //プレイヤーの移動はFixedUpdateでないとスムーズにならない模様。
     void FixedUpdate()
     {
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        UpdateAnimationAndMove();
+        if (currentState == PlayerState.walk) {
+            UpdateAnimationAndMove();
+        }
+    }
+
+    //Part11で追加
+    //GetButtonDownはFixedUpdateだと反応が悪い。
+    //http://mediamonster.blog.fc2.com/blog-entry-23.html
+    private void Update() {
+        if (Input.GetButtonDown("attack") && currentState != PlayerState.attack) {
+            StartCoroutine(AttackCo());//コルーチン
+        }
+    }
+
+    private IEnumerator AttackCo() {
+        animator.SetBool("attacking", true);
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(0.3f);//0.1f待つ
+        currentState = PlayerState.walk;
     }
 
     void UpdateAnimationAndMove() {
