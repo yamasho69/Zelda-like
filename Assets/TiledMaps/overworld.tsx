1ec1ab5cb14bb1a04f7da962d7bf7065094271aa
yamasho69 <nitiham55@yahoo.co.jp>
2020-07-23 21:26:44 +0900

    20200723 001
    タイルマップを増やした。

--- Assets/TiledMaps/overworld.tsx
+++ Assets/TiledMaps/overworld.tsx
@@ -35,6 +35,188 @@
  <tile id="576" terrain="2,2,,2"/>
  <tile id="615" terrain="2,,2,2"/>
  <tile id="616" terrain=",2,2,2"/>
+ <tile id="870">
+  <animation>
+   <frame tileid="338" duration="100"/>
+   <frame tileid="339" duration="100"/>
+   <frame tileid="340" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="871">
+  <animation>
+   <frame tileid="298" duration="100"/>
+   <frame tileid="299" duration="100"/>
+   <frame tileid="300" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="872">
+  <animation>
+   <frame tileid="258" duration="100"/>
+   <frame tileid="259" duration="100"/>
+   <frame tileid="260" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="874">
+  <animation>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="96" duration="100"/>
+   <frame tileid="98" duration="100"/>
+   <frame tileid="100" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="875">
+  <animation>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="97" duration="100"/>
+   <frame tileid="99" duration="100"/>
+   <frame tileid="101" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="877">
+  <animation>
+   <frame tileid="123" duration="100"/>
+   <frame tileid="124" duration="100"/>
+   <frame tileid="125" duration="100"/>
+   <frame tileid="163" duration="100"/>
+   <frame tileid="164" duration="100"/>
+   <frame tileid="165" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="910">
+  <animation>
+   <frame tileid="377" duration="100"/>
+   <frame tileid="417" duration="100"/>
+   <frame tileid="458" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="911">
+  <animation>
+   <frame tileid="378" duration="100"/>
+   <frame tileid="418" duration="100"/>
+   <frame tileid="459" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="912">
+  <animation>
+   <frame tileid="379" duration="100"/>
+   <frame tileid="419" duration="100"/>
+   <frame tileid="460" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="914">
+  <animation>
+   <frame tileid="56" duration="100"/>
+   <frame tileid="58" duration="100"/>
+   <frame tileid="60" duration="100"/>
+   <frame tileid="136" duration="100"/>
+   <frame tileid="138" duration="100"/>
+   <frame tileid="140" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="915">
+  <animation>
+   <frame tileid="57" duration="100"/>
+   <frame tileid="59" duration="100"/>
+   <frame tileid="61" duration="100"/>
+   <frame tileid="137" duration="100"/>
+   <frame tileid="139" duration="100"/>
+   <frame tileid="141" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="917">
+  <animation>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="80" duration="100"/>
+   <frame tileid="81" duration="100"/>
+   <frame tileid="82" duration="100"/>
+   <frame tileid="83" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="990">
+  <animation>
+   <frame tileid="1163" duration="100"/>
+   <frame tileid="1165" duration="100"/>
+   <frame tileid="1167" duration="100"/>
+   <frame tileid="1169" duration="100"/>
+   <frame tileid="1171" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="991">
+  <animation>
+   <frame tileid="1164" duration="100"/>
+   <frame tileid="1166" duration="100"/>
+   <frame tileid="1168" duration="100"/>
+   <frame tileid="1170" duration="100"/>
+   <frame tileid="1172" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1030">
+  <animation>
+   <frame tileid="1203" duration="100"/>
+   <frame tileid="1205" duration="100"/>
+   <frame tileid="1207" duration="100"/>
+   <frame tileid="1209" duration="100"/>
+   <frame tileid="1211" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1031">
+  <animation>
+   <frame tileid="1204" duration="100"/>
+   <frame tileid="1206" duration="100"/>
+   <frame tileid="1208" duration="100"/>
+   <frame tileid="1210" duration="100"/>
+   <frame tileid="1212" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1072">
+  <animation>
+   <frame tileid="382" duration="100"/>
+   <frame tileid="385" duration="100"/>
+   <frame tileid="388" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1073">
+  <animation>
+   <frame tileid="383" duration="100"/>
+   <frame tileid="386" duration="100"/>
+   <frame tileid="389" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1074">
+  <animation>
+   <frame tileid="384" duration="100"/>
+   <frame tileid="387" duration="100"/>
+   <frame tileid="390" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1112">
+  <animation>
+   <frame tileid="422" duration="100"/>
+   <frame tileid="425" duration="100"/>
+   <frame tileid="428" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1113">
+  <animation>
+   <frame tileid="423" duration="100"/>
+   <frame tileid="426" duration="100"/>
+   <frame tileid="429" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1114">
+  <animation>
+   <frame tileid="424" duration="100"/>
+   <frame tileid="427" duration="100"/>
+   <frame tileid="430" duration="100"/>
+  </animation>
+ </tile>
  <tile id="1160" terrain=",,,0"/>
  <tile id="1161" terrain=",,0,0"/>
  <tile id="1162" terrain=",,0,"/>
