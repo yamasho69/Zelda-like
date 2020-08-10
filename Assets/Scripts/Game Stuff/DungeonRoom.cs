4e35db7defd59da76f3da7949a401fd0cf8da840
yamasho69 <nitiham55@yahoo.co.jp>
2020-08-10 16:52:03 +0900

    20200810 002
    Part46まで完了。別の画面に切り替わると、壺と敵が復活する。ただし、蘇った敵が起きない不具合。

---
+++ Assets/Scripts/Game Stuff/DungeonRoom.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+//Part46で作成。敵をすべて倒さないと次にいけない部屋？
+public class DungeonRoom : MonoBehaviour{
+    
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
