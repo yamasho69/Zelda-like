ff50c26efd44f7a52c01e436a09bc17e6ead0445
yamasho69 <nitiham55@yahoo.co.jp>
2020-08-11 00:10:17 +0900

    20200811 001
    Part47最初。ダンジョンを作成・追加。

---
+++ Assets/Scripts/Game Stuff/DungeonEnemyRoom.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+//Part47で作成。
+public class DungeonEnemyRoom : MonoBehaviour{
+    
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
